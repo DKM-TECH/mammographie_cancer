425eeaa9057d962596db876d2a15fa654b8e2c5b
DKM-TECH <doschelah@gmail.com>
2026-07-07 19:15:43 +0200

    Fix tensorflow lazy loading gradcam

--- main.py
+++ main.py
@@ -54,12 +54,10 @@ app.mount(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5500",
-        "http://127.0.0.1:5500",
-
-        "http://localhost:8000",
-        "http://127.0.0.1:8000"
-    ],
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+    "null"
+],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
