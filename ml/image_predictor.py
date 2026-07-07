1ad0d0d6ca9d1839b9ab6b422884ed78ebacde36
DKM-TECH <doschelah@gmail.com>
2026-07-07 17:30:41 +0200

    Fix tensorflow lazy loading gradcam

--- ml/image_predictor.py
+++ ml/image_predictor.py
@@ -1,7 +1,6 @@
-import tensorflow as tf
 import numpy as np
 from pathlib import Path
-from tensorflow.keras.applications.efficientnet import preprocess_input
+
 
 BASE_DIR = Path(__file__).resolve().parent
 
@@ -10,25 +9,63 @@ MODEL_PATH = BASE_DIR / "cancer_model.h5"
 print("MODEL PATH =", MODEL_PATH)
 print("EXISTS ?", MODEL_PATH.exists())
 
+
 model = None
 
 IMG_SIZE = 224
 
 
+# ==========================
+# CHARGEMENT MODELE LAZY
+# ==========================
+
 def get_model():
+
     global model
+
     if model is None:
-        model = tf.keras.models.load_model(str(MODEL_PATH))
+
+        import tensorflow as tf
+
+        print("Chargement du modèle EfficientNet...")
+
+        model = tf.keras.models.load_model(
+            str(MODEL_PATH)
+        )
+
+        print("Modèle chargé")
+
     return model
 
 
+
+# ==========================
+# PREPROCESS IMAGE
+# ==========================
+
 def preprocess_image(image_file):
-    img = tf.keras.utils.load_img(image_file, target_size=(IMG_SIZE, IMG_SIZE))
+
+    import tensorflow as tf
+    from tensorflow.keras.applications.efficientnet import preprocess_input
+
+    img = tf.keras.utils.load_img(
+        image_file,
+        target_size=(IMG_SIZE, IMG_SIZE)
+    )
+
     img = tf.keras.utils.img_to_array(img)
-    img = preprocess_input(img)  # ✅ IMPORTANT
+
+    img = preprocess_input(img)
+
     img = np.expand_dims(img, axis=0)
+
     return img
 
+
+
+# ==========================
+# PREDICTION
+# ==========================
 
 def predict_image(img_path):
 
@@ -36,24 +73,34 @@ def predict_image(img_path):
 
     img = preprocess_image(img_path)
 
-    prediction = model.predict(img, verbose=0)
+    prediction = model.predict(
+        img,
+        verbose=0
+    )
 
     score = float(prediction[0][0])
 
+
     if score >= 0.5:
+
         prediction_label = "MALIGNE"
         diagnosis = "Cancer malin détecté"
         confidence = score
         class_id = 1
+
     else:
+
         prediction_label = "BENIGNE"
         diagnosis = "Aucune tumeur maligne détectée"
         confidence = 1 - score
         class_id = 0
 
+
     return {
+
         "prediction": prediction_label,
         "diagnostic": diagnosis,
         "confidence": confidence,
         "class_id": class_id
+
     }
