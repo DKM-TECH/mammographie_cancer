f9ccb10856c4804ea239bc3b0bbc451487029ccc
DKM-TECH <doschelah@gmail.com>
2026-07-07 22:54:09 +0200

    Fix tensorflow lazy loading gradcam

--- routes/diagnostics.py
+++ routes/diagnostics.py
@@ -186,9 +186,12 @@ async def predict_gradcam(file: UploadFile = File(...)):
 
     except Exception as e:
         import traceback
-        traceback.print_exc()
 
-        raise HTTPException(
-            status_code=500,
-            detail=str(e)
-        )
+        print("========== ERREUR COMPLETE ==========", flush=True)
+        traceback.print_exc()
+        print("=====================================", flush=True)
+
+        return {
+        "status": "error",
+        "detail": str(e)
+        }
